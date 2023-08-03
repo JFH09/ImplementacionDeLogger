b5dd476f5be4eef98ba36186057a1e5a83165523
JFH09 <julanfzh@gmail.com>
2023-08-02 19:06:05 -0500

    Se incluye un sistema de niveles implementando logger a partir del uso de winston con la siguiente prioridad de menor a mayor (debug, http, info, warning, error, fatal), de igual forma, se de implementa un logger para desarrollo y produccion, el cual se tomara trayendo el ambiente desde una variable de entorno, el logger de desarrollo loggea a partir del nivel de debug en la consola, y el logger de produccion loggea a partir del nivel info en consola, en ambos ambientes se crea un archivo errors.log que imprime niveles de log desde warning en ambos casos, se añaden algunos logs por el aplicativo en su mayoria sustitutos de console.log, los cuales loggean actividades realizadas en la aplicacion, y se crea la ruta /testLogger para imprimir los niveles de logger y probarlos.

---
+++ src/controllers/sessions.controller.js
@@ -0,0 +1,80 @@
+import userModel from "../dao/models/user.js";
+import User from "../dao/models/user.dao.js";
+
+const userServiceDAO = new User();
+
+const gitHub = async (req, res) => {};
+const githubcallback = async (req, res) => {
+  console.log("usuario recibe gitHub -> ", req.user);
+  let idUser = JSON.stringify(req.user._id);
+  let id = "";
+  id = idUser.split('"');
+  console.log(id[1]);
+  req.session.user = {
+    first_name: req.user.first_name,
+    last_name: req.user.last_name,
+    email: req.user.email,
+    id: id[1],
+    age: req.user.age,
+  };
+  console.log(req.session.user);
+  res.redirect("/api/products");
+};
+
+const register = async (req, res) => {
+  console.log("Entro a registrar un user...");
+  res.status(201).json({ status: "success", message: "Usuario Registrado" });
+};
+
+const login = async (req, res) => {
+  //console.log("entro a loguearse, ", req.user);
+  req.logger.info("ENTRO A LOGUEARSE");
+  if (!req.user) {
+    req.logger.error("No se inicio sesion, credenciales invalidas!!!!!!");
+    return res
+      .status(400)
+      .send({ status: "error", error: "credenciales invalidas" });
+  }
+
+  let result = await userServiceDAO.login(req);
+  console.log(result);
+  if (result.status == "success") {
+    req.logger.info("Se inicio sesion!!!!!!");
+    return res.send(result);
+    // } else {
+    //   req.logger.error("No se inicio sesion!!!!!!");
+    //   return res.send(401).json({ status: "error", error: error });
+  }
+};
+
+const getInfoUserById = async (req, res) => {
+  //console.log("buscando info usuario...");
+  req.logger.info("buscando info usuario...!!!!!!");
+  let id = req.params.id;
+  console.log("130", id);
+  let result = await userServiceDAO.getInfoUserById(id);
+  if (result.status != "error") {
+    return res.status(201).json(result);
+  } else {
+    return res.send(401).json({ status: "error", error: error });
+  }
+};
+
+const logoutSession = (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.json({ status: "Logout error", body: err });
+    }
+    res.status(201).json({ status: "success", payload: "logout ok!" });
+    console.log("Se cerro la sesion correctamente...");
+  });
+};
+
+export default {
+  gitHub,
+  githubcallback,
+  register,
+  login,
+  getInfoUserById,
+  logoutSession,
+};
